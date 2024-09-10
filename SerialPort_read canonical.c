3d3913d69ac786f1f591149fb6af05ea4e191b92
Beagle User <debian@BeagleBone.localdomain>
2024-09-10 00:20:38 +0000

    push de la partie 1 lab5

--- SerialPort_read canonical.c
+++ SerialPort_read canonical.c
@@ -61,8 +61,8 @@ void main(void)
 	
 	SerialPortSettings.c_iflag &= ~(IXON | IXOFF | IXANY);          // Disable XON/XOFF flow control both i/p and o/p
 
-	SerialPortSettings.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);  // Non Cannonical mode, Disable echo, Disable signal  
-
+	SerialPortSettings.c_lflag &= ~(ECHO | ECHOE | ISIG);  // Non Cannonical mode, Disable echo, Disable signal  
+    SerialPortSettings.c_lflag |= (ICANON);
 	SerialPortSettings.c_oflag &= ~OPOST;	// No Output Processing
 
 	// Setting Time outs 
